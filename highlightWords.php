cea65921d7cae07490bd9ec34b96e63a287cbd05
Maksim Pevnev <max.stak.job@gmail.com>
2022-07-13 20:29:57 +0400

    highlightWords utf-8

--- highlightWords.php
+++ highlightWords.php
@@ -1,3 +1,4 @@
+
 <?php
 require './vendor/autoload.php';
 
@@ -10,11 +11,12 @@ use Ds\Set;
  * Ну и прочая грамматика естественно не учтена, склонения и т. д..
  * 
  */
-function highlightWords(string $text, array $array_of_words): string {
+function highlightWords(string $text, array $array_of_words, ?callable $strtolowerCallable = null): string {
+    $strtolowerCallable = $strtolowerCallable ?? strtolower(...);
     $set_of_words = new Set();
     $set_of_highlighted_words = new Set();
     foreach ($array_of_words as $word) {
-        $set_of_words->add(strtolower($word));
+        $set_of_words->add($strtolowerCallable($word));
     }
 
     $result_text = '';
@@ -23,7 +25,7 @@ function highlightWords(string $text, array $array_of_words): string {
         $l = $text[$i];
         
         if ($l === ' ') {
-            $result_text .= highlightWord($word, $set_of_words, $set_of_highlighted_words);
+            $result_text .= highlightWord($word, $set_of_words, $set_of_highlighted_words, $strtolowerCallable);
             $result_text .= ' ';
             $word = '';
             continue;
@@ -31,13 +33,13 @@ function highlightWords(string $text, array $array_of_words): string {
 
         $word .= $l;
     }
-    $result_text .= highlightWord($word, $set_of_words, $set_of_highlighted_words);
+    $result_text .= highlightWord($word, $set_of_words, $set_of_highlighted_words, $strtolowerCallable);
 
     return $result_text;
 }
 
-function highlightWord(string $word, Set $set_of_words, Set $set_of_highlighted_words) {
-    $lowercased_word = strtolower($word);
+function highlightWord(string $word, Set $set_of_words, Set $set_of_highlighted_words, $strtolowerCallable) {
+    $lowercased_word = $strtolowerCallable($word);
     if (
         !$set_of_highlighted_words->contains($lowercased_word) && 
         $set_of_words->contains($lowercased_word)
@@ -48,6 +50,50 @@ function highlightWord(string $word, Set $set_of_words, Set $set_of_highlighted_
     return  "$word";
 }
 
+function strtolower_utf8($word) {
+    $alphabet = [
+        'а' => 'А',
+        'б' => 'Б',
+        'в' => 'В',
+        'г' => 'Г',
+        'д' => 'Д',
+        'е' => 'Е',
+        'ё' => 'Ё',
+        'ж' => 'Ж',
+        'з' => 'З',
+        'и' => 'И',
+        'й' => 'Й',
+        'к' => 'К',
+        'л' => 'Л',
+        'м' => 'М',
+        'н' => 'Н',
+        'о' => 'О',
+        'п' => 'П',
+        'р' => 'Р',
+        'с' => 'С',
+        'т' => 'Т',
+        'у' => 'У',
+        'ф' => 'Ф',
+        'х' => 'Х',
+        'ц' => 'Ц',
+        'ч' => 'Ч',
+        'ш' => 'Ш',
+        'щ' => 'Щ',
+        'ъ' => 'Ъ',
+        'ы' => 'Ы',
+        'ь' => 'Ь',
+        'э' => 'Э',
+        'ю' => 'Ю',
+        'я' => 'Я',
+    ];
+    $result_word = '';
+    foreach (preg_split('//u', $word, -1, PREG_SPLIT_NO_EMPTY) as $l) {
+        $result_word .= $alphabet[$l] ?? $l;
+    }
+    return $result_word;
+}
+
+
 var_dump(
     highlightWords(
         'Mama mYla ramu mama',
@@ -55,4 +101,14 @@ var_dump(
             'mama', 'Ramu',
         ]
     ) === '[Mama] mYla [ramu] mama'
+);
+
+var_dump(
+    highlightWords(
+        'Мама мЫла раму мама',
+        [
+            'мама', 'Раму',
+        ],
+        strtolower_utf8(...)
+    ) === '[Мама] мЫла [раму] мама'
 );
